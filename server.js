1db047b77ea474ef01fc80132c86e5d15b29e712
Vipun Sanjana Jayathunga <92296561+vipunsanjana@users.noreply.github.com>
2024-03-03 01:09:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,9 +8,12 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path  from "path";
-
+import {fileURLToPath} from "url";
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //databse config
 connectDB();
